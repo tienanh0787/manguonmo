7709a28df5696d98f8acffbe58ea848cd9e62516
TIEN ANH NGUYEN PHONG <1821050787@student.humg.edu.vn>
2021-09-27 08:32:00 +0700

    message

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -8,11 +8,16 @@ namespace MvcMovie.Models
         [Key]
         
         public int Id { get; set; }
+        [Required(ErrorMessage ="Title is required")]
+        [MaxLength(15)]
+        [MinLength(3)]
         public string Title { get; set; }
-
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
         public string Genre { get; set; }
+
         public decimal Price { get; set; }
+
     }
 }
